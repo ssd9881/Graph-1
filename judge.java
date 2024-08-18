2cea8093fff267a116a0912494b6f124adb838a8
sdumbre1 <sdumbre@charlotte.edu>
2024-08-18 12:01:37 -0400

    Done Graph-1

---
+++ judge.java
@@ -0,0 +1,17 @@
+// tc:O(v+e) sc:O(v)
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int [] indegrees = new int [n+1];
+        for(int [] edge:trust){
+            indegrees[edge[0]]--;
+            indegrees[edge[1]]++;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(indegrees[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
